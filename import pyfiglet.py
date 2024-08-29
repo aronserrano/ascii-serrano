4f98451cf0425169bc7711243ac08f91994d9f7b
aronserrano <aronmarcusserrano27@gmail.com>
2024-08-29 19:08:22 +0800

    Add files via upload

---
+++ import pyfiglet.py
@@ -0,0 +1,8 @@
+import pyfiglet
+
+text = pyfiglet.print_figlet(text="ARON", colors="RED" , font="SLANT")
+
+text = pyfiglet.print_figlet(text="MARCUS", colors="GREEN" , font="SLANT")
+
+text = pyfiglet.print_figlet(text="SERRANO", colors="BLUE" , font="SLANT")
+print(text)
